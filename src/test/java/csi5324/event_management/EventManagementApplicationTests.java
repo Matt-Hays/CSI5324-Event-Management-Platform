3a6817034fd6e66a36fc8011681a6811b140259a
MattHays2 <mhays@distorted.dev>
2024-03-03 18:34:23 -0700

    Inital Commit

---
+++ src/test/java/csi5324/event_management/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package csi5324.event_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
