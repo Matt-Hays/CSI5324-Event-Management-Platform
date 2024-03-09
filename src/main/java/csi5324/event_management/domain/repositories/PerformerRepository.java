f2a3d2c58a4bbe128e906b269bcbe20d8b5c6dbc
MattHays2 <mhays@distorted.dev>
2024-03-08 23:57:16 -0700

    Created base repo and services files. Implemented Event Service.

---
+++ src/main/java/csi5324/event_management/domain/repositories/PerformerRepository.java
@@ -0,0 +1,2 @@
+package csi5324.event_management.domain.repositories;public interface PerformerRepository {
+}
---
+++ src/main/java/csi5324/event_management/services/PerformerService.java
@@ -0,0 +1,2 @@
+package csi5324.event_management.services;public class PerformerService {
+}
